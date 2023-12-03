e688eefaaba82a27a42b87dc14012ad934c15d99
maxnoahb <maxnoahb@gmail.com>
2023-12-03 15:39:10 -0500

    day 2 part 2

--- day2.py
+++ day2.py
@@ -1,7 +1,28 @@
+from functools import reduce
+import operator
+
 MAX = {"red": 12, "green": 13, "blue": 14}
 
 
 def parse_input():
+    """
+    Input format:
+        Game 1: 8 green; 5 green, 6 blue, 1 red; 2 green, 1 blue, 4 red
+        Game 2: 10 blue, 12 red; 8 red; 7 green, 5 red, 7 blue
+    Output format: (take the highest value for each color in each game)
+        {
+            1: {
+                "green": 8,
+                "blue": 6,
+                "red": 4
+            },
+            2: {
+                "blue": 10,
+                "red": 12,
+                "green": 7
+            }
+        }
+    """
     with open("day2_input.txt", "r") as f:
         input_list = f.read().splitlines()
     input_dict = {}
@@ -22,17 +43,26 @@ def parse_input():
 
 
 def solve_part1(input_dict):
-    sum = 0
+    cubes = 0
     for id, game in input_dict.items():
         possible = True
         for color in list(game.keys()):
             if game[color] > MAX[color]:
                 possible = False
         if possible:
-            sum += id
-    print(sum)
+            cubes += id
+    print(cubes)
+
+
+def solve_part2(input_dict):
+    cubes = 0
+    for _, game in input_dict.items():
+        num_cubes = reduce(operator.mul, game.values())
+        cubes += num_cubes
+    print(cubes)
 
 
 if __name__ == "__main__":
     input_dict = parse_input()
     solve_part1(input_dict)
+    solve_part2(input_dict)
