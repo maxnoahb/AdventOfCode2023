eccaed4f1b179f61728aa1a4f10501b9e95dc860
maxnoahb <maxnoahb@gmail.com>
2023-12-09 16:14:52 -0500

    day 8 part 2

--- day8.py
+++ day8.py
@@ -1,8 +1,14 @@
-from ast import literal_eval
+import math
 
 
 def parse_input():
-    """Parse input into"""
+    """
+    Parse input into a string containing the left-right instructions, and a dict with format
+        {
+            "AAA": ["BBB", "CCC"],
+            "BBB": ["ZZZ", "ABC"]
+        }
+    """
     lines = open("inputs/day8_input.txt").read().strip().splitlines()
     lr_instructions = lines[0]
     nodes = {}
@@ -16,6 +22,10 @@ def parse_input():
 
 
 def solve_part1(lr_instructions, nodes):
+    """
+    Loop until we hit a ZZZ node, resetting the node on each loop to the proper new node
+    Increase the index of the left-right instructions on each loop and keep a running count of steps
+    """
     cur = "AAA"
     i = 0
     num_steps = 0
@@ -30,6 +40,32 @@ def solve_part1(lr_instructions, nodes):
     print(num_steps)
 
 
+def solve_part2(lr_instructions, nodes):
+    """
+    Identify each node we want to start from, then loop through those nodes
+    For each node to track, follow similar logic to part 1 to find the number of steps
+        it takes until we reach a node ending in a Z
+    Append to a list that keeps the number of steps necessary for each node,
+        then find the LCM (least common multiple) of the list
+    """
+    nodes_to_track = [i for i in list(nodes.keys()) if i[-1] == "A"]
+    steps = []
+    for node in nodes_to_track:
+        i = 0
+        num_steps = 0
+        while node[2] != "Z":
+            if lr_instructions[i] == "L":
+                node = nodes[node][0]
+            elif lr_instructions[i] == "R":
+                node = nodes[node][1]
+            num_steps += 1
+            i = i + 1 if i < (len(lr_instructions) - 1) else 0
+        steps.append(num_steps)
+
+    print(steps, math.lcm(*steps))
+
+
 if __name__ == "__main__":
     lr_instructions, nodes = parse_input()
     solve_part1(lr_instructions, nodes)
+    solve_part2(lr_instructions, nodes)
